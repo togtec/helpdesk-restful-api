33fdbe3e87ce9b2a54b33da014e0a26c04713d67
rdgtog <rdgtog@gmail.com>
2023-05-28 23:00:15 -0300

    aula 24 - Autenticação - e. Criar classe UserDetailsServiceImpl conforme contrato do Spring Security

---
+++ src/main/java/com/rodrigo/helpdesk/services/UserDetailsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.rodrigo.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.helpdesk.domain.Pessoa;
+import com.rodrigo.helpdesk.repositories.PessoaRepository;
+import com.rodrigo.helpdesk.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	private final PessoaRepository repository;	
+
+	public UserDetailsServiceImpl(PessoaRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = repository.findByEmail(email);
+		if (user.isPresent()) 
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+			
+		throw new UsernameNotFoundException(email);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
